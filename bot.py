30d325fba13a9821107c9a5d68ce8214f0b09055
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-10 21:03:03 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,17 +11,40 @@ logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
 server = web.AppRunner(web_server())
 
-async def start_services():
-    await web.TCPSite(server, 8000).start()
-
 BOT_TOKEN = os.environ.get("TOKEN", "1763065907:AAEtGmMbHR8lZTY5xn0XYtm9JWAp_Zga0OY")
 API_ID = int(os.environ.get("API_ID",2766365))
 API_HASH = os.environ.get("API_HASH", "b867ccbeb57dd4f0c8e1d82e8bc363ef")
+PORT= int(os.environ.get("PORT", 8000))
+app = Client("MYBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID, plugins={"root": "plugins"})
+loop = asyncio.get_event_loop()
 
-if __name__ == "__main__" :
-    plugins = dict(root="plugins")
-    app = Client("MYBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID, plugins=plugins)
-    logger.info("Bot Started")
-    app.run()
-    idle()
-    logger.info("Bot Stopped")
+async def start_services():
+    print()
+    print("-------------------- Initializing Telegram Bot --------------------")
+    await app.start()
+    bot_info = await app.get_me()
+    app.username = bot_info.username
+    print("------------------------------ DONE ------------------------------")
+    await server.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(server, bind_address, PORT).start()
+    print("------------------------------ DONE ------------------------------")
+    print()
+    print("------------------------- Service Started -------------------------")
+    await idle()
+
+async def cleanup():
+    await server.cleanup()
+    await app.stop()
+
+if __name__ == "__main__":
+    try:
+        loop.run_until_complete(start_services())
+    except KeyboardInterrupt:
+        pass
+    except Exception as err:
+        logging.error(err.with_traceback(None))
+    finally:
+        loop.run_until_complete(cleanup())
+        loop.stop()
+        print("Stopped Services")
