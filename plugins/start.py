7858c356c97974f5f4d34285e909d34bc1e27a0c
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-10 14:55:52 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,9 +1,7 @@
-from pyrogram import filters
-from pyrogram.types import Message
-from bot import StreamBot
+from pyrogram import Client, filters
 
-@StreamBot.on_message(filters.command(["start", "help"]))
-async def start(_, m: Message):
-    await m.reply(
-        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
-    )
+@Client.on_message(filters.private & filters.command(['start']))
+async def start(client, message):
+          insert(int(message.chat.id))
+          await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id , parse_mode="markdown", reply_markup=InlineKeyboardMarkup(            [                [                    InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ],                 [InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical"),InlineKeyboardButton("How To Use",url = "https://youtu.be/dUYvenXiYKE") ]           ]        ) )
+      
