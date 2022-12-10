3ba07656ea39632174a57eb60505368c88d5ac36
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-10 14:50:37 +0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from os import environ
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config(object):
-    API_ID = int(environ.get("API_ID", "2766365"))
-    API_HASH = str(environ.get("API_HASH", "b867ccbeb57dd4f0c8e1d82e8bc363ef"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN", "1763065907:AAEtGmMbHR8lZTY5xn0XYtm9JWAp_Zga0OY"))
