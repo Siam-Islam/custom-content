75c2caf22564b20125bcab1e334ec54da27d2f33
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-10 14:17:10 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,25 @@
+import os
+import logging
 from config import Config
 from pyrogram import Client
 
-StreamBot = Client(
-    name="custom-content",
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    plugins={"root": "plugins"},
-    bot_token=Config.BOT_TOKEN,
-)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+if __name__ == "__main__":
+
+    StreamBot = Client(
+                name="custom-content",
+                api_id=Config.API_ID,
+                api_hash=Config.API_HASH,
+                plugins={"root": "plugins"},
+                bot_token=Config.BOT_TOKEN,)
+
+    logger.info("Bot Started")
+    StreamBot.run()
+    idle()
+    logger.info("Bot Stopped")
+
