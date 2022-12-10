966fc7a9f7b434281e34f83698b1ffb9442295e5
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-10 14:20:45 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,1 +1,9 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from bot import StreamBot
 
+@StreamBot.on_message(filters.command(["start", "help"]))
+async def start(_, m: Message):
+    await m.reply(
+        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
+    )
