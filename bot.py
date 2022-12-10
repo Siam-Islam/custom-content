cc087f0cb31d54b06ede92057927025cacc794d7
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-10 14:11:13 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
+from config import Config
+from pyrogram import Client
 
+StreamBot = Client(
+    name="custom-content",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    plugins={"root": "plugins"},
+    bot_token=Config.BOT_TOKEN,
+)
