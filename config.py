3c7d30b93c26e26b7e2a80ef9606fd1b9a4f9e32
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-10 14:05:43 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Var(object):
+class Config(object):
     API_ID = int(environ.get("API_ID", "2766365"))
     API_HASH = str(environ.get("API_HASH", "b867ccbeb57dd4f0c8e1d82e8bc363ef"))
     BOT_TOKEN = str(environ.get("BOT_TOKEN", "5848398486:AAE5POD1kHSQC1F6eIhmzIyqdihUM2wDkVY"))
