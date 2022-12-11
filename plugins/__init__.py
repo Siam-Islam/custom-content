2b82ab1692d2ba8c593c5cea92e5b7ab7fecde79
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-11 09:04:30 +0600

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,7 +1,13 @@
 from aiohttp import web
-from .stream import routes
+
+routes = web.RouteTableDef()
 
 def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(_):
+    return web.json_response({"server_status": "running",})
+
