007357569ebb5e11ef56aefc91ab007bc3452ba8
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-11 09:32:22 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,4 @@
-from pyrogram.types import filters, Client, Message
+from pyrogram import filters, Client, Message
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(_m: Message):
