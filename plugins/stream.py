7ef97e8a1e972a5273aec3ba669bd6bc3e7cd6c4
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-11 09:02:57 +0600

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -1,9 +1,1 @@
-from aiohttp import web
 
-routes = web.RouteTableDef()
-
-@routes.get("/", allow_head=True)
-async def root_route_handler(_):
-    return web.json_response({
-            "server_status": "running",       
-        })
