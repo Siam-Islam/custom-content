ed2c59b56dfd1956f7d0957484c55c6c6d2d83e4
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-11 09:52:15 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,5 @@
 from pyrogram import filters, Client, Message
 
-@Client.on_message(filters.private & filters.command(['start']))
+@Client.on_message(filters.command(["start", "help"]))
 async def start(_, m: Message):
-    await m.reply('Hi I am working.') 
+    await m.reply(f'Hi, Send me a file to get an instant stream link.')
