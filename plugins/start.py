3161234151153f6d7ae041ff4c28286a1d1debe0
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-11 09:33:29 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,5 @@
 from pyrogram import filters, Client, Message
 
 @app.on_message(filters.private & filters.command(['start']))
-async def start(_m: Message):
+async def start(_, m: Message):
     await m.reply('Hi I am working.') 
