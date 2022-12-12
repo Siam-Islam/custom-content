2b352b397890bb9cc49304b4c74d790ea4f08ee2
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-12 23:08:12 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,3 +3,7 @@ from pyrogram import Client as app, filters
 @app.on_message(filters.command(["start", "help"]))
 async def start(client, message):
     await message.reply('Hi, Send me a file to get an instant stream link.')
+
+@app.on_message(filters.private &( filters.document | filters.audio | filters.video ))
+async def start(client, message):
+    await message.reply('Hello')
