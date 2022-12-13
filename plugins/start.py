700b37aff7c0af42131ab949d43872b610d2ebde
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-13 16:33:11 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,3 +1,5 @@
+import humanize
+from pyrogram.file_id import FileId
 from pyrogram import Client as app, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
 
@@ -7,6 +9,11 @@ async def start(client, message):
 
 @app.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def send_doc(client, message):
-    await message.reply(f"""What do you want me to do with this file?""",reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup(
+    media = await client.get_messages(message.chat.id, message.message_id)
+    file = media.document or media.video or media.audio
+    filename = file.file_name
+    filesize = humanize.naturalsize(file.file_size)
+    fileid = file.file_id
+    await message.reply(f"""What do you want me to do with this file?\n**File Name** :- {filename}\n**File Size** :- {filesize}""",reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup(
        		[[ InlineKeyboardButton("📝 Rename", callback_data = "rename"),
        		InlineKeyboardButton("✖️ Cancel", callback_data = "cancel")  ]]))
