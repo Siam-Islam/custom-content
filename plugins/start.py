3cc64a88d7e2d20e2a9f96420f09e5e729107a00
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-13 11:34:49 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,6 +4,21 @@ from pyrogram import Client as app, filters
 async def start(client, message):
     await message.reply('Hi, Send me a file to get an instant stream link.')
 
+@app.on_callback_query(filters.regex('cancel'))
+async def cancel(bot,update):
+	try:
+		await update.message.delete()
+	except:
+		return
+
+@app.on_callback_query(filters.regex('rename'))
+async def rename(bot,update):
+	chat_id = update.message.chat.id
+	id = update.message.reply_to_message_id
+	await update.message.delete()
+	await update.message.reply_text(f"__Please enter the new filename...__\n\nNote:- Extension Not Required",reply_to_message_id = id,
+	reply_markup=ForceReply(True)
+
 @app.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def doc(client, message):
     await message.reply(f"""What do you want me to do with this file?""",reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup(
