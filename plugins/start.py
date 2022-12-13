48213132657e7ecf3cbc213ad9f026f0eb2a6749
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-13 11:37:00 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,7 +20,7 @@ async def rename(bot,update):
 	reply_markup=ForceReply(True)
 
 @app.on_message(filters.private &( filters.document | filters.audio | filters.video ))
-async def doc(client, message):
+async def send_doc(client, message):
     await message.reply(f"""What do you want me to do with this file?""",reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup(
        		[[ InlineKeyboardButton("📝 Rename", callback_data = "rename"),
        		InlineKeyboardButton("✖️ Cancel", callback_data = "cancel")  ]]))
