9ef507b0b8c1d99951803d7539c8410bb5693283
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-13 11:48:16 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,9 +1,5 @@
 from pyrogram import Client as app, filters
 
-@app.on_message(filters.command(["start", "help"]))
-async def start(client, message):
-    await message.reply('Hi, Send me a file to get an instant stream link.')
-
 @app.on_callback_query(filters.regex('cancel'))
 async def cancel(bot,update):
 	try:
@@ -12,12 +8,16 @@ async def cancel(bot,update):
 		return
 
 @app.on_callback_query(filters.regex('rename'))
-async def rename(bot,update):
+async def rename(bot, update):
 	chat_id = update.message.chat.id
 	id = update.message.reply_to_message_id
 	await update.message.delete()
 	await update.message.reply_text(f"__Please enter the new filename...__\n\nNote:- Extension Not Required",reply_to_message_id = id,
 	reply_markup=ForceReply(True)
+
+@app.on_message(filters.command(["start", "help"]))
+async def start(client, message):
+    await message.reply('Hi, Send me a file to get an instant stream link.')
 
 @app.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def send_doc(client, message):
