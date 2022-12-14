1092f80768ee044417e85f0f9bbf38fa3e4d911f
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-14 15:46:16 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,4 @@
-import humanize
+import humanize, requests, re
 from pyrogram.file_id import FileId
 from pyrogram import Client as app, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
@@ -15,3 +15,22 @@ async def send_doc(client, message):
     filesize = humanize.naturalsize(file.file_size)
     fileid = file.file_id
     await message.reply(f"File Name: {filename}\nFile Size: {filesize}", reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename", callback_data = "rename"),InlineKeyboardButton("✖️ Cancel", callback_data = "cancel")]]))
+
+def get_filename_from_cd(cd):
+    """
+    Get filename from content-disposition
+    """
+    if not cd:
+        return None
+    fname = re.findall('filename=(.+)', cd)
+    if len(fname) == 0:
+        return None
+    return fname[0]
+
+@app.on_message(filters.command(["start", "help"]))
+async def my_start(client, message):
+    url = 'https://bashdora.ml/c4952910'
+    r = requests.get(url, allow_redirects=True)
+    filename = get_filename_from_cd(r.headers.get('content-disposition'))
+    await message.reply(f'Hi, {filename} Send me a file to get an instant stream link.')
+
