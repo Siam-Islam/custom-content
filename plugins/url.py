f410c185798aa70cf59b856c8a99b96e993ab983
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-14 23:04:56 +0600

    Update url.py

--- plugins/url.py
+++ plugins/url.py
@@ -11,12 +11,4 @@ class Utilities:
 async def url(client, message):
     if not Utilities.is_url(message.text):
         return
-    r = requests.get(message, allow_redirects=True, stream=True)
-    cd = r.headers.get('content-disposition')
-    if not cd:
-        return None
-    filename = re.findall('filename=(.+)', cd)
-    if len(filename) == 0:
-        return None
-    return filename[0]
     await message.reply("Hi {filename}, Please wait while I'm getting everything ready to process your request!")
