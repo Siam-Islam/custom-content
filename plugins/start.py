bf7f5a19cbee9c63ec895cb94a7b140ae47ba02b
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-15 11:27:13 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,3 @@
-import humanize
 from pyrogram.file_id import FileId
 from pyrogram import Client as app, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
@@ -6,13 +5,4 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 @app.on_message(filters.command(["start", "help"]))
 async def start(client, message):
     await message.reply("Hi, me a file to get an instant stream link.")
-
-@app.on_message(filters.private &( filters.document | filters.audio | filters.video ))
-async def send_doc(client, message):
-    media = await client.get_messages(message.chat.id, message.id)
-    file = media.document or media.video or media.audio
-    filename = file.file_name
-    filesize = humanize.naturalsize(file.file_size)
-    fileid = file.file_id
-    await message.reply(f"File Name: {filename}\nFile Size: {filesize}", reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename", callback_data = "rename"),InlineKeyboardButton("✖️ Cancel", callback_data = "cancel")]]))
 
