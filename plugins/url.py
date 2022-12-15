f668653b667bf60ecef25674b228c5705e5cf18c
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-15 11:23:19 +0600

    Update url.py

--- plugins/url.py
+++ plugins/url.py
@@ -1,5 +1,7 @@
-import requests, re, humanize
+import requests, re
 from pyrogram import Client as app, filters
+
+SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
 class Utilities:
     def is_url(text):
@@ -22,6 +24,18 @@ class Utilities:
            return filesize
        return "Unable to get file size."
 
+    def get_readable_file_size(size_in_bytes) -> str:
+    if size_in_bytes is None:
+        return '0B'
+    index = 0
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
+        index += 1
+    try:
+        return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
+    except IndexError:
+        return 'File too large'
+
 @app.on_message(filters.private & filters.text)
 async def url(client, message):
     if not Utilities.is_url(message.text):
@@ -30,7 +44,7 @@ async def url(client, message):
     file_link = message.text
     name = await Utilities.get_filename(file_link)
     bytes = await Utilities.get_filesize(file_link)
-    size = humanize.naturalsize(bytes)
+    size = get_readable_file_size(bytes)
     if isinstance(name, str):
         await snt.edit_text("😟 Sorry! I cannot open the file.")
         return
