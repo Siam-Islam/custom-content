cb923905943f53b81abfeff9ae1ffebf02220c92
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-15 18:39:50 +0600

    Update url.py

--- plugins/url.py
+++ plugins/url.py
@@ -8,9 +8,9 @@ class utilities:
     def get_filename(file_link):
        r = requests.get(file_link, allow_redirects=True, stream=True)
        cd = r.headers.get('content-disposition')
-       filename = re.findall('filename=(.+)', cd)
+       filename = re.findall('filename=(.+)', cd)[0]
        if filename:
-           return filename[0]
+           return filename
        return None
 
     def get_filesize(file_link):
