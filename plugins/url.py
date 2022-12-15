ab97e3c46bc45412d6680aa780b7756e659b441c
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-15 18:14:44 +0600

    Update url.py

--- plugins/url.py
+++ plugins/url.py
@@ -2,10 +2,10 @@ import requests, re, humanize
 from pyrogram import Client as app, filters
 
 class utilities:
-    async def is_url(text):
+    def is_url(text):
         return text.startswith("http")
 
-    async def get_filename(file_link):
+    def get_filename(file_link):
        r = requests.get(file_link, allow_redirects=True, stream=True)
        cd = r.headers.get('content-disposition')
        filename = re.findall('filename=(.+)', cd)
@@ -13,12 +13,12 @@ class utilities:
            return filename
        return None
 
-    async def get_filesize(file_link):
+    def get_filesize(file_link):
        r = requests.get(file_link, allow_redirects=True, stream=True)
        filesize = r.headers.get("Content-Length", 0)
        if filesize:
-           return filesize
-       return "Unable to get file size."
+           return filesize[0]
+       return None
 
 @app.on_message(filters.private & filters.text)
 async def url(client, message):
@@ -26,8 +26,8 @@ async def url(client, message):
         return
     snt = await message.reply("Hi Please wait while I'm getting everything ready to process your request!")
     file_link = message.text
-    name = await utilities.get_filename(file_link)
-    bytes = await utilities.get_filesize(file_link)
+    name = utilities.get_filename(file_link)
+    bytes = utilities.get_filesize(file_link)
     size = humanize.naturalsize(bytes, binary=True)
     if isinstance(name, str):
         await snt.edit_text("😟 Sorry! I cannot open the file.")
