17d459e94401d54ea85fd15149db2efeafe0f8f9
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-16 11:39:34 +0600

    Update url.py

--- plugins/url.py
+++ plugins/url.py
@@ -9,16 +9,12 @@ class utilities:
        r = requests.get(file_link, allow_redirects=True, stream=True)
        cd = r.headers.get('content-disposition')
        filename = re.findall('filename=(.+)', cd)
-       if filename:
-           return filename
-       return
+       return filename[0]
 
     async def get_filesize(file_link):
        r = requests.get(file_link, allow_redirects=True, stream=True)
        filesize = r.headers.get("Content-Length", 0)
-       if filesize:
-           return filesize
-       return
+       return filesize
 
 @app.on_message(filters.private & filters.text)
 async def url(client, message):
@@ -29,8 +25,8 @@ async def url(client, message):
     name = await utilities.get_filename(file_link)
     bytes = await utilities.get_filesize(file_link)
     size = humanize.naturalsize(bytes, binary=True)
-    if isinstance(bytes, str):
-        await snt.edit_text("😟 Sorry! I cannot open the file.")
+    if isinstance(name, str):
+        await snt.edit_text(f"😟 Sorry! I {name} cannot open the file.")
         return
     await snt.edit_text(f"Title: {name}\nSize: {size}")
         
