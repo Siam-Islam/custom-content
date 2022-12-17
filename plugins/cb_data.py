2458dfb8890f29cccae3c5af5884833e8465c5eb
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-17 12:33:59 +0600

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -1,15 +1,36 @@
-import asyncio
+import os, asyncio, time
+from helper.progress import progress_for_pyrogram
 from pyrogram import Client as app, filters
 
 @app.on_callback_query(filters.regex('cancel'))
 async def cancel(bot,update):
         await update.message.delete()
 
-@app.on_callback_query(filters.regex('rename'))
-async def rename(bot,update):
-	chat_id = update.message.chat.id
-	id = update.message.reply_to_message_id
-	await update.message.delete()
-	await update.message.reply_text(f"__Please enter the new filename...__\n\nNote:- Extension Not Required",reply_to_message_id = id,
-	reply_markup=ForceReply(True) )
-
+@app.on_callback_query(filters.regex("doc"))
+async def doc(bot,update):
+     new_name = update.message.text
+     name = new_name.split(":-")
+     new_filename = name[1]
+     file_path = f"downloads/{new_filename}"
+     file = update.message.reply_to_message
+     ms = await update.message.edit("``` Trying To Download...```")
+     c_time = time.time()
+     try:
+     	path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "``` Trying To Download...```",  ms, c_time   ))
+     except Exception as e:
+     	await ms.edit(e)
+     	return
+     	
+     splitpath = path.split("/downloads/")
+     dow_file_name = splitpath[1]
+     await ms.edit("```Trying To Upload```")
+     		c_time = time.time()
+     		try:
+     			await bot.send_document(update.message.chat.id,document = file_path,caption = f"**{new_filename}**",progress=progress_for_pyrogram,progress_args=( "```Trying To Uploading```",  ms, c_time   ))
+     			await ms.delete()
+     			os.remove(file_path)
+     		except Exception as e:
+     			await ms.edit(e)
+     			os.remove(file_path)
+     
+     		
