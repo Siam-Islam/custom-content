a45b2b8b970706184a5fce07140947a20e53adf7
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-19 16:33:27 +0600

    Update url.py

--- plugins/url.py
+++ plugins/url.py
@@ -33,9 +33,7 @@ async def url(client, message):
     await message.delete()
     if not name == "None":
         await snt.edit_text(f"Title: {name[1:][:-1]}\nSize: {size}", reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Upload",callback_data = "doc"),InlineKeyboardButton("Cancel",callback_data = "cancel") ]]))
-            return
     else:
         await snt.edit_text("Unsupported link!")
-            return
   
         
