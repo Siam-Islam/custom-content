9372c357425292145273b61ead78e9913dc32cc1
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-19 17:09:10 +0600

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -1,15 +1,26 @@
-import os, asyncio, time
+import os, asyncio, time, requests, re
 from helper.progress import progress_for_pyrogram
 from pyrogram import Client as app, filters
 
+class utilities:
+    async def get_filename(file):
+       r = s.get(text, allow_redirects=True, stream=True)
+       cd = r.headers.get("content-disposition")
+       if not cd:
+           return "None"
+       fname = re.findall("filename=(.+)", cd)
+       name = fname[0]
+       return name
+
 @app.on_callback_query(filters.regex("cancel"))
-async def cancel(bot,update):
+async def cancel(bot, update):
         await update.message.delete()
 
 @app.on_callback_query(filters.regex("doc"))
-async def doc(bot,update):
-     file_path = f"downloads/video.mp4"
+async def doc(bot, update):
      file = update.message.text
+     name = await utilities.get_filename(file)
+     file_path = f"downloads/{name[1:][:-1]}"
      ms = await update.message.edit_text("Trying To Download...")
      try:
      	 await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=("Trying To Download...",  ms, time.time()))
