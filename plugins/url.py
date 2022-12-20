fb3da25e791a9b037ca363931bfdb2364b3f91f0
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-20 12:46:53 +0600

    Update url.py

--- plugins/url.py
+++ plugins/url.py
@@ -5,8 +5,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 s = requests.Session()
 
 class utilities:
-    async def get_filename(file_link):
-       r = s.get(file_link, allow_redirects=True, stream=True)
+    async def get_filename(url):
+       r = s.get(url, allow_redirects=True, stream=True)
        cd = r.headers.get("content-disposition")
        if not cd:
            return "None"
@@ -14,26 +14,24 @@ class utilities:
        name = fname[0]
        return name
 
-    async def get_filesize(file_link):
-       r = s.get(file_link, allow_redirects=True, stream=True)
+    async def get_filesize(url):
+       r = s.get(url, allow_redirects=True, stream=True)
        filesize = r.headers.get("Content-Length", 0)
        return filesize
 
 @app.on_message(filters.private & filters.text)
 async def url(client, message):
-    file_link = message.text
-    https_link = file_link.startswith("https://")
-    http_link = file_link.startswith("http://")
-    if not https_link or http_link:
+    url = message.text
+    if not url.startswith("https://") or url.startswith("http://"):
         return
     snt = await message.reply("Processing link.......")
-    name = await utilities.get_filename(file_link)
-    bytes = await utilities.get_filesize(file_link)
+    name = await utilities.get_filename(url)
+    bytes = await utilities.get_filesize(url)
     size = humanize.naturalsize(bytes, binary=True)
     await message.delete()
     if not name == "None":
-        await snt.edit(f"Title: {name[1:][:-1]}\nSize: {size}", reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Upload",callback_data = "doc"),InlineKeyboardButton("Cancel",callback_data = "cancel") ]]))
+        await snt.edit_text(f"Title: {name[1:][:-1]}\nSize: {size}", reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Upload",callback_data = "doc"),InlineKeyboardButton("Cancel",callback_data = "cancel") ]]))
     else:
-        await snt.edit("Unsupported link!")
+        await snt.edit_text("Unsupported link!")
   
         
