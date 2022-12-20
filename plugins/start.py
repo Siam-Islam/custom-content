21fda92c1dc26c6dd3dfc94e724861c25213a2ad
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-20 22:48:38 +0600

    Delete start.py

--- plugins/start.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.file_id import FileId
-from pyrogram import Client as app, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
-
-@app.on_message(filters.command(["start", "help"]))
-async def start(client, message):
-    await message.reply("Hi, me a file to get an instant stream link.")
-
