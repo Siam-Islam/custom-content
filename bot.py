117906e00a5c1f205650792f10de3bcb5a013616
Siam islam <77488919+Siam-Islam@users.noreply.github.com>
2022-12-20 22:08:31 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os, asyncio, logging
 from aiohttp import web
-from pyrogram import Client, idle
+from pyrogram import Client
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -8,10 +8,10 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
-token = os.environ.get("TOKEN", "1763065907:AAFk-ITNJWExRGAzCSGzKkchUTLqP7lHFxQ")
+bot_token = os.environ.get("TOKEN", "1763065907:AAFk-ITNJWExRGAzCSGzKkchUTLqP7lHFxQ")
 api_id = os.environ.get("API_ID", "2766365")
 api_hash = os.environ.get("API_HASH", "b867ccbeb57dd4f0c8e1d82e8bc363ef")
-app = Client("MYBot", bot_token=token, api_hash=api_hash, api_id=api_id, plugins={"root": "plugins"})
+app = Client("my_bot", bot_token=bot_token, api_hash=api_hash, api_id=api_id, plugins={"root": "plugins"})
 port = os.environ.get("PORT", "8000")
 loop = asyncio.get_event_loop()
 routes = web.RouteTableDef()
@@ -33,7 +33,6 @@ async def start_services():
     await server.setup()
     await web.TCPSite(server, "0.0.0.0", port).start()
     print("--BOT STARTED SUCESSFULLY--")
-    await idle()
 
 if __name__ == "__main__":
     try:
